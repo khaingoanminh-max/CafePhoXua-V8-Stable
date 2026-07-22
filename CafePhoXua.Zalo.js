9fc9c5b5e8dc9f57d1dc4986f0a167aa0777b589
khaingoanminh-max <khaingoanminh@gmail.com>
2026-07-22 08:20:08 +0700

    Update CafePhoXua.Zalo.js

--- CafePhoXua.Zalo.js
+++ CafePhoXua.Zalo.js
@@ -1,1 +1,19 @@
+/**
+ * =====================================================
+ * Cafe Phố Xưa - Zalo Connector
+ * -----------------------------------------------------
+ * Chịu trách nhiệm kết nối website với Zalo.
+ *
+ * Chức năng:
+ * - Mã hóa nội dung đơn hàng
+ * - Tạo URL Zalo
+ * - Gửi đơn hàng sang Zalo
+ *
+ * Không xử lý:
+ * - Giao diện
+ * - Giỏ hàng
+ * - Tạo nội dung đơn hàng
+ * =====================================================
+ */
 
+const CafePhoXuaZalo = {};
